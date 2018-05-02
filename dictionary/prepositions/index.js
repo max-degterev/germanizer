ea945e180286fc5ad0425faa3dadcae69cb9ed2c
Max Degterev <max@degterev.me>
2018-05-02 19:52:34 +0200

    prepositions dict

---
+++ dictionary/prepositions/index.js
@@ -0,0 +1,237 @@
+module.exports = [
+  // Accusative
+  {
+    word: 'betreffend',
+    translate: '(Accusative) concerning, with reference to',
+  },
+  {
+    word: 'bis',
+    translate: '(Accusative) until, by',
+  },
+  {
+    word: 'durch',
+    translate: '(Accusative) through, by, because of, via',
+  },
+  {
+    word: 'für',
+    translate: '(Accusative) for',
+  },
+  {
+    word: 'entlang',
+    translate: '(Accusative) along',
+  },
+  {
+    word: 'gegen',
+    translate: '(Accusative) against, around/about (a time)',
+  },
+  {
+    word: 'ohne',
+    translate: '(Accusative) without',
+  },
+  {
+    word: 'um',
+    translate: '(Accusative) around, at (a time)',
+  },
+  {
+    word: 'wider',
+    translate: '(Accusative) against',
+  },
+
+  // Accusative/Dative
+  {
+    word: 'an',
+    translation: '(Accusative/Dative) at, on',
+  },
+  {
+    word: 'auf',
+    translation: '(Accusative/Dative) on, up',
+  },
+  {
+    word: 'hinter',
+    translation: '(Accusative/Dative) behind',
+  },
+  {
+    word: 'in',
+    translation: '(Accusative) into, to, (Dative) in',
+  },
+  {
+    word: 'neben',
+    translation: '(Accusative/Dative) next to (not just near)',
+  },
+  {
+    word: 'über',
+    translation: '(Accusative/Dative) over, above, about',
+  },
+  {
+    word: 'unter',
+    translation: '(Accusative/Dative) under, below, among',
+  },
+  {
+    word: 'vor',
+    translation: '(Accusative/Dative) in front of, of, before',
+  },
+  {
+    word: 'zwischen',
+    translation: '(Accusative/Dative) between',
+  },
+
+  // Dative
+  {
+    word: 'ab',
+    translations: '(Dative) from (a point in time)',
+  },
+  {
+    word: 'aus',
+    translations: '(Dative) from (origin or source / out of / made of)',
+  },
+  {
+    word: 'außer',
+    translations: '(Dative) except for, besides, apart from, out of',
+  },
+  {
+    word: 'bei',
+    translations: '(Dative) at, with/at the house of',
+  },
+  {
+    word: 'entgegen',
+    translations: '(Dative) towards, contrary to, notwithstanding',
+  },
+  {
+    word: 'entsprechend',
+    translations: '(Dative) considering, according to, corresponding to',
+  },
+  {
+    word: 'gegenüber',
+    translations: '(Dative) across from, to (a person)',
+  },
+  {
+    word: 'gemäß',
+    translations: '(Dative) according to, in accordance with (often used legally)',
+  },
+  {
+    word: 'laut',
+    translations: '(Dative) according to',
+  },
+  {
+    word: 'mit',
+    translations: '(Dative) with, over (time), at (an age)',
+  },
+  {
+    word: '(mit)samt',
+    translations: '(Dative) including, together with',
+  },
+  {
+    word: 'nach',
+    translations: '(Dative) after, according to, to',
+  },
+  {
+    word: 'nahe',
+    translations: '(Dative) near',
+  },
+  {
+    word: 'seit',
+    translations: '(Dative) since (point in time), for (time period)',
+  },
+  {
+    word: 'von',
+    translations: '(Dative) from (immediate origin), of, by',
+  },
+  {
+    word: 'zu',
+    translations: '(Dative) to, at, too (adverb)',
+  },
+
+  // Genitive
+  {
+    word: '(an)statt',
+    translation: '(Genitive) instead of',
+  },
+  {
+    word: 'abzüglich',
+    translation: '(Genitive) less, minus',
+  },
+  {
+    word: 'angesichts',
+    translation: '(Genitive) in view of, given',
+  },
+  {
+    word: 'außerhalb',
+    translation: '(Genitive) outside of',
+  },
+  {
+    word: 'bar',
+    translation: '(Genitive) devoid of, barring any',
+  },
+  {
+    word: 'bezüglich',
+    translation: '(Genitive) (bzgl.)  concerning, regarding, with respect to',
+  },
+  {
+    word: 'binnen',
+    translation: '(Genitive) within (a time period)',
+  },
+  {
+    word: 'dank',
+    translation: '(Genitive) thanks to (also frequently used in the dative)',
+  },
+  {
+    word: 'einschließlich',
+    translation: '(Genitive) including',
+  },
+  {
+    word: 'infolge',
+    translation: '(Genitive) due to, because of',
+  },
+  {
+    word: 'inmitten',
+    translation: '(Genitive) in the middle/midst of',
+  },
+  {
+    word: 'innerhalb',
+    translation: '(Genitive) inside of',
+  },
+  {
+    word: 'mangels',
+    translation: '(Genitive) for lack of (esp. legal)',
+  },
+  {
+    word: 'mittels',
+    translation: '(Genitive) by means of (esp. technical)',
+  },
+  {
+    word: 'oberhalb',
+    translation: '(Genitive) above, beyond',
+  },
+  {
+    word: 'unbeschadet',
+    translation: '(Genitive) notwithstanding',
+  },
+  {
+    word: 'ungeachtet',
+    translation: '(Genitive) in spite of',
+  },
+  {
+    word: 'unterhalb',
+    translation: '(Genitive) below, underneath',
+  },
+  {
+    word: 'unweit',
+    translation: '(Genitive) not far from',
+  },
+  {
+    word: 'zugunsten',
+    translation: '(Genitive) in favor of, in the interests of',
+  },
+  {
+    word: 'trotz',
+    translation: '(Genitive) despite',
+  },
+  {
+    word: 'während',
+    translation: '(Genitive) during',
+  },
+  {
+    word: 'wegen',
+    translation: '(Genitive) because of',
+  },
+];
