db9556946cad847c3778192a3725f94f8979ec32
Max Degterev <max@degterev.me>
2020-09-25 20:43:23 +0200

    fix server start

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,7 +12,28 @@ module.exports = {
       ref: 'origin/master',
       repo: 'git@github.com:suprmax/germanizer.git',
       path: '/home/germanizer/app',
-      'post-deploy': 'npm ci && npm run compile',
+      'post-deploy': 'npm ci && npm run gulp build',
     },
   },
+
+  apps: [
+    {
+      name: 'germanizer',
+      script: './app.js',
+      kill_timeout: 1000,
+      exp_backoff_restart_delay: 500,
+      wait_ready: true,
+      time: true,
+      'merge-logs': true,
+      instances: 1,
+      exec_mode: 'cluster_mode',
+      max_memory_restart: '800M',
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
 };
